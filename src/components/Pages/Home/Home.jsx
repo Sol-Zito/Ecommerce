6d4f7c936deab8c18d862f83275924ff1703a1f0
Sol <solzito.1889@gmail.com>
2023-04-04 22:37:31 -0300

    remove userContext en home

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -6,37 +6,6 @@ import { UserContext } from "../../../Context/UserContext/UserContext";
 
 const Home = () => {
   const navigate = useNavigate();
-  // // userContext basados en usuarios de git,
-  // //ver como usarlo.modificarlo para lograr usar una propia api
-  // const userContext = useContext(UserContext);
-  // const { user, changeUser } = userContext;
-  // const [input, setInput] = useState(user.username);
-
-  // const [change, setChange] = useState(false);
-
-  // const onClick = async () => {
-  //   const data = await fetch(`https://api.github.com/users/${input}`);
-  //   const json = await data.json();
-
-  //   console.log(json.message);
-
-  //   if (!json.message) {
-  //     const { name, avatar_url, html_url, login } = json;
-
-  //     changeUser({ name, avatar_url, html_url, username: login });
-  //     setChange(true);
-  //   } else {
-  //     alert("no existe");
-  //     setChange(false);
-  //     setInput("");
-  //   }
-
-  //   //const [json1, setJson1] = useState({});
-  //   // const dataA = axios.get(`https://api.github.com/users/${input}`);
-  //   // dataA.then((res) => setJson1(res.data));
-  //   // console.log(json1);
-  // };
-  // userContext es hasta aca
 
   const loginFunction = () => {
     console.log("ya termine el proceso");
@@ -46,32 +15,6 @@ const Home = () => {
     <>
       <br></br>
       <h1>Bienvenido al home</h1>
-      <br></br>
-
-      {/* En este div esta lo del useContext ver de modificarlo*/}
-      {/* <div>
-        <div>
-          <input
-            placeholder="Ingresa el nombre de usuario"
-            defaultValue={user.username}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <button onClick={onClick}>Ver Perfil</button>
-        </div>
-
-        {change && (
-          <section>
-            <button>
-              <a href={user.html_url} target="_blank" rel="noreferrer">
-                Ver perfil completo
-              </a>
-            </button>
-            <h3>Nombre: {user.name}</h3>
-            <h4>Alias: {user.username}</h4>
-            <img src={user.avatar_url} alt={user.name} />
-          </section>
-        )}
-      </div> */}
 
       <br></br>
       <Button variant="contained" onClick={loginFunction} color="success">
